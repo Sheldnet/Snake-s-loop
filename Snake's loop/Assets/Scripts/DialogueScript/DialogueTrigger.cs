b5857a540a7c736fae86dc156a144762b174eaee
dextery <zupnikegor@gmail.com>
2022-04-07 21:55:06 +0300

    Dialogue system added and interactible things

---
+++ Snake's loop/Assets/Scripts/DialogueScript/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Conversation convo;
+    public BoxCollider2D trigger;
+
+    public void StartConvo()
+    {
+        DialogueManager.StartConversation(convo);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Snakeboi")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                DialogueManager.StartConversation(convo);
+            }
+        }
+    }
+}
---
+++ Snake's loop/Assets/Scripts/DialogueScript/SceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public Conversation maintalk;
+    public BoxCollider2D trigger;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Snakeboi")
+        {
+            DialogueManager.StartConversation(maintalk);
+            trigger.enabled = false;
+        }
+    }
+}
