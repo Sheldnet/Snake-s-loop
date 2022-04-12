a81e6acc66bcc565b4395e6486c6218677c194ba
dextery <zupnikegor@gmail.com>
2022-04-12 12:47:34 +0300

    Minus sound + everything else

---
+++ Snake's loop/Assets/Scripts/CutsceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class CutsceneChanger : MonoBehaviour
+{
+    public Animator anim;
+    public int scenIndex;
+    public GameObject scenebox;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (scenebox.activeSelf==false)
+            {
+                StartCoroutine(LoadScene());
+            }
+        }
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        anim.SetTrigger("End");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(scenIndex);
+    }
+}
